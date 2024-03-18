5b965d09537ee9bcaa6a35aff238900a8b6adb39
Gustavo Castro <gstvcastroc@gmail.com>
2024-03-18 19:04:13 -0300

    versao atualizada front mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent{
+  currentDate = new Date();
+
+  public months = [
+    { id: 0, name: 'Janeiro' },
+    { id: 1, name: 'Fevereiro' },
+    { id: 2, name: 'Março' },
+    { id: 3, name: 'Abril' },
+    { id: 4, name: 'Maio' },
+    { id: 5, name: 'Junho' },
+    { id: 6, name: 'Julho' },
+    { id: 7, name: 'Agosto' },
+    { id: 8, name: 'Setembro' },
+    { id: 9, name: 'Outubro' },
+    { id: 10, name: 'Novembro' },
+    { id: 11, name: 'Dezembro' },
+  ];
+  
+  public currentMonth : number = this.currentDate.getMonth();
+  
+  dateString = this.months.find(i => i.id === this.currentMonth)?.name + ' ' + this.currentDate.getFullYear();
+
   title = 'CervejoApp';
 }
